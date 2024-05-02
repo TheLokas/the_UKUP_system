02003171b62fb0a6332637d42dfa28514cc90d58
telexhs <telexhs@gmail.com>
2024-05-02 14:04:42 +0300

    Добавление компетенций и страницы без дизайна

---
+++ app/UKUP/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField, SelectField, EmailField, PasswordField, Field, SelectMultipleField, TextAreaField, SubmitField, HiddenField
+from wtforms.validators import input_required, data_required, ValidationError, Length
+
+
+class DisciplineForm(FlaskForm):
+    name = StringField()
+    year_approved = SelectField()
+    block = SelectField()
+    module = SelectField()
+    department = SelectField()
+    direction = SelectMultipleField()
+    submit = SubmitField()
+
+    def addData(self, year, block, module, department, direction):
+        self.year_approved.choices = year
+        self.block.choices = block
+        self.module.choices = module
+        self.department.choices = department
+        self.direction.choices = direction
+
+
+class CompetenceForm(FlaskForm):
+    name = StringField()
+    num = IntegerField()
+    year_approved = SelectField()
+    type = SelectField(choices=["УК", "ОПК", "ПК"])
+    formulation = TextAreaField()
+    submit = SubmitField()
+
+    def addData(self, year):
+        self.year_approved.choices = year
