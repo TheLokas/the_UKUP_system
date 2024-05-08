10a17ffb3cd4a57693c2be940f5647a68f9eeb1b
gruzd <gruzdev-2002@bk.ru>
2024-05-09 00:42:37 +0300

    Добавлена функция получения всех модулей

--- app/UKUP/templates/moduleDB.py
+++ app/UKUP/templates/moduleDB.py
@@ -1,6 +1,16 @@
-from app.models import Block
+from app.models import Block, Module
 
+
+# Получаем все блоки из базы данных
 def get_block():
     blocks = Block.query.all()
     return blocks
+
+
+# Получаем все модули из базы данных
+def get_modules():
+    modules = Module.query.all()
+    return modules
+
+
 
