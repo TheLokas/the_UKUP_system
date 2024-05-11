ca947cbc94b39625e74f79864dcf3d057e58c8f9
Alxej <alexey.merkushev.2003@gmail.com>
2024-05-11 17:22:03 +0300

    исправлена функция создания связи дисциплины и компетенции, добавлена функция редактирования связи дисциплины и компетенции

--- app/UKUP/module_db.py
+++ app/UKUP/module_db.py
@@ -1,8 +1,9 @@
 from app.models import db, Competence, Discipline, CompetenceDiscipline
-from datetime import date
 
 
-def connect_discipline_with_competence(competence_id, discipline_id):
+def connect_discipline_with_competence(competence_id: int,
+                                       discipline_id: int,
+                                       year: int):
     competence = Competence.query.get(competence_id)
     discipline = Discipline.query.get(discipline_id)
 
@@ -11,10 +12,9 @@ def connect_discipline_with_competence(competence_id, discipline_id):
     if not discipline:
         return None
 
-    current_year = date.today().year
     comp_to_dis_ref = CompetenceDiscipline(competence_id=competence_id,
                                            discipline_id=discipline_id,
-                                           year_created=current_year)
+                                           year_created=year)
     try:
         db.session.add(comp_to_dis_ref)
         db.session.commit()
@@ -22,3 +22,31 @@ def connect_discipline_with_competence(competence_id, discipline_id):
         return None
 
     return comp_to_dis_ref
+
+
+def edit_discipline_and_competence_connection(ref_id: int,
+                                              new_discipline_id: int = None,
+                                              new_competence_id: int = None,
+                                              new_year: int = None):
+    ref = CompetenceDiscipline.query.get(ref_id)
+    if not ref:
+        return None
+
+    try:
+        if new_discipline_id is not None:
+            ref.discipline_id = new_discipline_id
+            db.session.flush()
+
+        if new_competence_id is not None:
+            ref.competence_id = new_competence_id
+            db.session.flush()
+
+        if new_year is not None:
+            ref.year_created = new_year
+            db.session.flush()
+
+        db.session.commit()
+    except Exception:
+        return None
+
+    return ref
