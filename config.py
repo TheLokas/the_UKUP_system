54b3df74ef46b93b9c2f82645dcba7f7c2343ca3
gruzd <gruzdev-2002@bk.ru>
2024-05-21 22:33:04 +0300

    Кнопки скачивания

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = 'mysql://gen_user:^wVTtDGXXsiF36@147.45.140.206:3306/UKUP'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 
